364aac0ab0fe7473e1e462d4d202c68704e79a99
yipeng <yip@augurit.com>
2023-11-03 10:28:08 +0800

    代码优化

--- day_01/src/components/use_effect/countdown.tsx
+++ day_01/src/components/use_effect/countdown.tsx
@@ -1,24 +1,10 @@
-import React, { useEffect, useState } from "react"
-
-const Countdown = () => {
-    const [count, setCount] = useState(10)
-    useEffect(() => {
-        const timerId = setInterval(() => {
-            setCount(prev => {
-                if (prev === 1) {
-                    clearInterval(timerId)
-                }
-                return prev - 1
-            })
-        }, 1000)
-    }, [])
-    return count
-}
+import React from "react"
+import { useCountDown } from "@/hooks"
 
 export const TestCountdown:React.FC = () => {
-    const timer = Countdown()
+    const [timer, disabled] = useCountDown(10)
     console.log(timer)
     return (<>
-        { timer >= 0 ? <button disabled>请仔细阅读本协议内容({ timer }) 秒</button> : <button onClick={() => console.log('成功激活')}>确认此协议</button> }
+        <button disabled={disabled} onClick={() => console.log('成功激活')}>{  disabled ? `请仔细阅读本协议内容(${ timer }) 秒` : '确认此协议'}</button>
     </>)
 }   
--- day_01/src/hooks/index.ts
+++ day_01/src/hooks/index.ts
@@ -19,3 +19,21 @@ export const useMouseInfo = (delay:number = 0) => {
     }, [])
     return position
 }
+type UseCountDown = (seconds: number) => [number, boolean]
+export const useCountDown:UseCountDown = (seconds = 10) => {
+    // 如果没传参数，默认参数是10， 如果传参是0则变为10
+    seconds = Math.round(Math.abs(seconds)) || 10
+    const [count, setCount] = useState(seconds)
+    const [disabled, setDisable] = useState(true) 
+    useEffect(() => {
+        setTimeout(() => {
+            if (count > 1) {
+                setCount(prev => prev - 1)
+            } else {
+                setDisable(false)
+            }
+        }, 1000)
+        // return () => clearTimeout(timerId)
+    }, [count])
+    return [count, disabled]
+}
